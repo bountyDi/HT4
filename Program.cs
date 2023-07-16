e19c63583c2e88b8ee80e67aed615c2a19689e10
Diana <dina1405@yandex.ru>
2023-07-16 18:38:55 +0600

    not final

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿int Input(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Task25()
+{
+    int num1 = Input("Enter first number");
+    int num2 = Input("Enter second number");
+
+    double result = Math.Pow(num1, num2);
+    Console.Write(result);
+}
+
+void Task27()
+{
+    int num = Input("Enter a number");
+    int sum = 0;
+    int counter = 0;
+    int digit = 0;
+    while (num > 0)
+    {
+        digit = num%10;
+        sum = sum + digit;
+        num = num/10;
+    }
+    Console.WriteLine(sum);
+}
+Task29()
+{
+    
+}
